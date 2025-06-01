19faaf6d08f71321ef5a68a5df533f6a7272fadb
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-05-31 23:24:25 -0700

    complete size method

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -58,7 +58,12 @@ public class TraversalPractice {
      * @return the number of nodes in the tree
      */
     public static int size(TreeNode node) {
-        return -1;
+        if (node == null) return 0;
+        
+        int count = 1;
+        count += size(node.left);
+        count += size(node.right);
+        return count;
     }
 
     /**
