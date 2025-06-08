9e6cd1b90ecaac750868c5a46d6359c8e94df4dd
Jessica H <jessicahebert75@gmail.com>
2025-06-07 19:14:38 -0700

    Implemented a method that sums the branch noes in the tree

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -61,7 +61,7 @@ public class TraversalPractice {
 
         if (node == null) return 0;
         
-        return -1;
+        return 1 + size(node.left) + size(node.right);
     }
 
     /**
@@ -88,7 +88,12 @@ public class TraversalPractice {
      * @return the count of branch nodes in the tree
      */
     public static int branchCount(TreeNode node) {
-        return -1;
+        if ( node == null) return 0;
+
+        if (node.left != null || node.right != null){
+            return 1+branchCount(node.left)+branchCount(node.right);
+        }
+        return 0;
     }
 
 
